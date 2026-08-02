1345c4e0bc85f9929f5b0108d992d951553b1fbb
Shafiun Nabi Riyad <vcoding153@gmail.com>
2026-08-02 22:29:16 +0600

    payment niye kaj korsi

--- main.c
+++ main.c
@@ -1,20 +1,6 @@
-void addMoney(int userIndex){
-    char agentNum[12];
-    double amount;
-    printf("\n~~~~~~~~~~>>ADD MONEY<<~~~~~~~~~~\n");
-    printf("Enter Agent Number: ");
-    scanf("%[^\n]",agentNum);
-
-    //validation Function - Riyad
-
-    printf("Enter Amount to Add: ");
-    scanf("%lf", &amount);
-
-    if(amount <= 0){
-        printf("Your amount is too low. Try again");
-        return;
-    }
-
-    //balace += amount;- Riyad
-
+#include<stdio.h>
+int main()
+{
+    //payment 
+    
 }
